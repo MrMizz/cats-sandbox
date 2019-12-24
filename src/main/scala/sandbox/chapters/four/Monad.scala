c1e3c4af66ca5c50fe65bc5b415cbd8637719edf
Alexander Binaei <alex.binaei@carpe.io>
2019-12-24 14:58:39 -0800

    Implemented Monad.map with pure and flatMap.

---
+++ src/main/scala/sandbox/chapters/four/Monad.scala
@@ -0,0 +1,13 @@
+package sandbox.chapters.four
+
+trait Monad[F[_]] {
+
+  def pure[A](a: A): F[A]
+
+  def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
+
+  def map[A, B](value: F[A])(func: A => B): F[B] = {
+    flatMap(value)(func andThen pure)
+  }
+
+}
